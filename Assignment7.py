bd760330247722bb38607edd3ee9369cea89097f
Md Mustakin Mahi <63896978+Mustakin-140@users.noreply.github.com>
2021-09-21 20:57:23 +0600

    Add files via upload

---
+++ Assignment7.py
@@ -0,0 +1,26 @@
+def power(x,y): 
+    if y==0:
+        return 1
+    if y%2 == 0:
+        return power(x,y//2) * power(x,y//2)
+    return x * power(x,y//2) * power(x,y//2)
+
+def order(x):
+    n = 0
+    while(x!=0):
+        n = n+1
+        x = x//10
+    return n
+
+def ArmStrong(x):
+    n = order(x)
+    temp = x
+    sum = 0
+    
+    while(temp!=0):
+        r  = temp % 10
+        sum = sum + power(r,n)
+        temp = temp//10
+    return (sum == x)
+x = 153
+print(ArmStrong(x))
